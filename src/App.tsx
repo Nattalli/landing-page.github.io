9a3416c3f97a05e19da09aa447ed895a2a23052a
Nataliia <dominok56@gmail.com>
2023-04-12 23:49:49 +0300

    created full project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.scss';
 import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import Landing from "./pages/landing/Landing";
 
 function App() {
   return (
     <div className="layout">
       <Header />
+        <Landing/>
+      <Footer/>
     </div>
   );
 }
